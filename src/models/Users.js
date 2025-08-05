2b7299ad25069c86e54883567074c74208465d75
SFarazH <75844866+SFarazH@users.noreply.github.com>
2025-08-05 15:42:24 +0530

    edit model
    Co-authored-by: Adina <adina01anwar@gmail.com>

--- src/models/Users.js
+++ src/models/Users.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     role: {
       type: String,
-      enum: ["admin", "doctor", "receptionist"],
+      enum: ["admin", "doctor", "receptionist", "pharmacist"],
       required: true,
     },
     phoneNumber: String,
