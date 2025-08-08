f12571cf593aaf883bc7ffe70ef489039248db05
SFarazH <75844866+SFarazH@users.noreply.github.com>
2025-08-08 11:05:34 +0530

    add to index

--- src/services/index.js
+++ src/services/index.js
@@ -3,4 +3,5 @@ export * from "./procedures.service";
 export * from "./doctors.service";
 export * from "./app-settings.service";
 export * from "./appointments.service";
-export * from './auth.service'
+export * from "./auth.service";
+export * from "./medicines.service";
