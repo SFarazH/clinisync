be5fc23b077f00bf3d5846316db1d8bb38d83533
SFarazH <75844866+SFarazH@users.noreply.github.com>
2025-08-12 12:12:03 +0530

    update prescreption schema

--- src/models/Prescreption.js
+++ src/models/Prescreption.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
+const prescriptionItemSchema = new mongoose.Schema({
+  medicine: { type: String, required: true },
+  frequency: { type: String, required: true },
+  duration: { type: Number, required: true },
+  instructions: { type: String },
+});
+
 const prescreptionSchema = new mongoose.Schema({
-  
+  medications: {
+    type: [prescriptionItemSchema],
+    required: true,
+  },
+  generalNotes: { type: String, required: false },
 });
 
 export default mongoose.models.Prescreption ||
