4d0a19d5023a845da2e317d78806b2646139f624
SFarazH <75844866+SFarazH@users.noreply.github.com>
2025-09-05 19:12:14 +0530

    edit payments model

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -1,36 +1,36 @@
-import mongoose from "mongoose";
+  import mongoose from "mongoose";
 
-const PaymentSchema = new mongoose.Schema(
-  {
-    appointment: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Appointment",
-      required: true,
-    },
-    patient: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Patient",
-      required: true,
-    },
-    amountPaid: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    amountPending: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    paymentsHistory: [
-      {
-        date: { type: Date, required: true, default: Date.now },
-        amount: { type: Number, required: true },
+  const PaymentSchema = new mongoose.Schema(
+    {
+      appointment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Appointment",
+        required: true,
       },
-    ],
-  },
-  { timestamps: true }
-);
+      patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient",
+        required: true,
+      },
+      amountPaid: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+      amountPending: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+      paymentsHistory: [
+        {
+          date: { type: Date, required: true, default: Date.now },
+          amount: { type: Number, required: true },
+        },
+      ],
+    },
+    { timestamps: true }
+  );
 
-export default mongoose.models.Payment ||
-  mongoose.model("Payment", PaymentSchema);
+  export default mongoose.models.Payment ||
+    mongoose.model("Payment", PaymentSchema);
