d7345b35885fc5bc714d415bbbe1a00e95355819
SFarazH <75844866+SFarazH@users.noreply.github.com>
2025-10-28 22:12:13 +0530

    init api

--- src/services/payments.service.js
+++ src/services/payments.service.js
@@ -1,4 +1,4 @@
-import Payment from "@/models/Payment";
+import Payment from "@/models/Invoice";
 import { dbConnect } from "@/utils/dbConnect";
 
 export async function addPayment(data) {
@@ -8,7 +8,53 @@ export async function addPayment(data) {
     const payment = await Payment.create(data);
     return { success: true, data: payment };
   } catch (error) {
-    console.error("Error creating payment:", error);
+    console.error("Error adding payment:", error);
+    return { success: false, error: error.message };
+  }
+}
+
+export async function getPayments({
+  transactionType = "income" | "expense",
+  patientId = null,
+  isPaymentComplete = null,
+  paginate = true,
+  page = 1,
+  limit = 10,
+}) {
+  await dbConnect();
+
+  try {
+    const query = {};
+    query.transactionType = transactionType;
+    if (patientId) {
+      query.patientId = patientId;
+    }
+
+    if (isPaymentComplete) {
+      query.isPaymentComplete = isPaymentComplete;
+    }
+
+    let paymentsQuery = Payment.find(query).populate("patientId", "name");
+    let total;
+    if (paginate) {
+      paymentsQuery = paymentsQuery.skip((page - 1) * limit).limit(limit);
+      total = await Payment.countDocuments(query);
+    }
+
+    const paymentData = await paymentsQuery;
+
+    const res = { success: true, data: paymentData };
+    if (paginate) {
+      res.pagination = {
+        total,
+        page,
+        pages: Math.ceil(total / limit),
+        limit,
+      };
+    }
+    return res;
+  } catch (error) {
+    console.error("Error fetching payments:", error);
     return { success: false, error: error.message };
   }
 }
