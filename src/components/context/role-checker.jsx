bbef9fa54e99439c5bbd0ee62b03d9cef2c489d6
SFarazH <75844866+SFarazH@users.noreply.github.com>
2025-11-13 17:28:12 +0530

    add role checkers

--- src/components/context/role-checker.jsx
+++ src/components/context/role-checker.jsx
@@ -1,10 +1,25 @@
+import { useRouter } from "next/navigation";
 import { useAuth } from "./authcontext";
 
-export const RoleBasedWrapper = ({ allowedRoles, children }) => {
-  const { authUser } = useAuth();
+export const RoleBasedWrapper = ({
+  allowedRoles,
+  featureKey = null,
+  children,
+}) => {
+  const { authUser, authClinic } = useAuth();
+  const router = useRouter();
 
-  if (!authUser) return null;
-  if (!allowedRoles.includes(authUser.role)) return null;
+  if (!authUser) {
+    return null;
+  }
+
+  if (allowedRoles.length > 0 && !allowedRoles.includes(authUser.role)) {
+    return null;
+  }
+
+  if (featureKey && authClinic?.features?.[featureKey] !== true) {
+    return null;
+  }
 
   return children;
 };
